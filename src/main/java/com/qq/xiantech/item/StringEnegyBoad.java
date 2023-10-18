2b919f99e8b252fcb68d8a5073a052ac1bd4d8d2
juzi45 <JuziDroid@outlook.com>
2023-10-18 22:22:48 +0800

    lang fix

--- src/main/java/com/qq/xiantech/item/StringEnegyBoad.java
+++ src/main/java/com/qq/xiantech/item/StringEnegyBoad.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.Item;
 public class StringEnegyBoad extends Item {
 
     /**
-     * 默认属性
+     * 默认属性 **需添加耐久对应能量属性
      */
     private final static Item.Properties DEFAULT_PROPERTIES = new Item.Properties();
 
